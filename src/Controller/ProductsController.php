9bbd5e8cb3e53bdffd8175493b28de8ac910107b
Daniel Yousefi Far <daniyousefifar@gmail.com>
2024-07-21 08:22:57 +0300

    02-DTO

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,21 +2,32 @@
 
 namespace App\Controller;
 
+use App\DTO\LowestPriceEnquiry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
-class ProductsController
+class ProductsController extends AbstractController
 {
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
-    public function lowestPrice(Request $request, int $id): Response
+    public function lowestPrice(Request $request, int $id, SerializerInterface $serializer): Response
     {
         if ($request->headers->has('force-fail')) {
             return new JsonResponse([
                 'error' => 'Promotions Engine failure message'
             ], $request->headers->get('force-fail'));
         }
+
+        // 1. Deserialize json data into EnquiryDTO
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+
+        // 2. Pass the Enquiry into a promotions filter the appropriate promotion will be applied
+        // 3. Return the modified Enquiry
+
+        dd($lowestPriceEnquiry);
 
         return new JsonResponse([
             'quantity' => 5,
