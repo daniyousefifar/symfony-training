81135990f5eb4fd636e0601cc950fd5291aba456
Daniel Yousefi Far <daniyousefifar@gmail.com>
2024-07-21 08:22:58 +0300

    03-Serializer

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,17 +3,17 @@
 namespace App\Controller;
 
 use App\DTO\LowestPriceEnquiry;
+use App\Service\Serializer\DTOSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductsController extends AbstractController
 {
     #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
-    public function lowestPrice(Request $request, int $id, SerializerInterface $serializer): Response
+    public function lowestPrice(Request $request, int $id, DTOSerializer $serializer): Response
     {
         if ($request->headers->has('force-fail')) {
             return new JsonResponse([
@@ -22,23 +22,18 @@ class ProductsController extends AbstractController
         }
 
         // 1. Deserialize json data into EnquiryDTO
+        /** @var LowestPriceEnquiry $lowestPriceEnquiry */
         $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
 
         // 2. Pass the Enquiry into a promotions filter the appropriate promotion will be applied
+        $lowestPriceEnquiry->setPrice(100);
+        $lowestPriceEnquiry->setDiscountedPrice(50);
+        $lowestPriceEnquiry->setPromotionId(3);
+        $lowestPriceEnquiry->setPromotionName('Black Friday half price sale');
+
         // 3. Return the modified Enquiry
+        $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json');
 
-        dd($lowestPriceEnquiry);
-
-        return new JsonResponse([
-            'quantity' => 5,
-            'request_location' => 'UK',
-            'voucher_code' => 'OU812',
-            'request_date' => '2024-07-21',
-            'product_id' => $id,
-            'price' => 100,
-            'discounted_price' => 50,
-            'promotion_id' => 3,
-            'promotion_name' => 'Black Friday half price sale',
-        ], 200);
+        return new Response($responseContent, 200);
     }
 }
