1077376b19a41a96c09d0c487e4e72c81eebf72d
Daniel Yousefi Far <daniyousefifar@gmail.com>
2024-07-21 12:49:44 +0300

    12-Price Modifiers 2

--- tests/unit/PriceModifiersTest.php
+++ tests/unit/PriceModifiersTest.php
@@ -5,6 +5,7 @@ namespace App\Tests\unit;
 use App\DTO\LowestPriceEnquiry;
 use App\Entity\Promotion;
 use App\Filter\Modifier\DateRangeMultiplier;
+use App\Filter\Modifier\EvenItemsMultiplier;
 use App\Filter\Modifier\FixedPriceVoucher;
 use App\Tests\ServiceTestCase;
 
@@ -54,5 +55,49 @@ class PriceModifiersTest extends ServiceTestCase
 
         // Then
         $this->assertEquals(500, $modifiedPrice);
+    }
+
+    /** @test */
+    public function EventItemsMultiplier_returns_a_correctly_modified_price(): void
+    {
+        // Given
+        $promotion = new Promotion();
+        $promotion->setName('Buy one get one free');
+        $promotion->setAdjustment(0.5);
+        $promotion->setCriteria(["minimum_quantity" => 2]);
+        $promotion->setType('even_items_multiplier');
+
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+
+        $evenItemsMultiplier = new EvenItemsMultiplier();
+
+        // When
+        $modifiedPrice = $evenItemsMultiplier->modify(100, 5, $promotion, $enquiry);
+
+        // Then
+        $this->assertEquals(300, $modifiedPrice);
+    }
+
+    /** @test */
+    public function EventItemsMultiplier_correctly_calculates_alternatives(): void
+    {
+        // Given
+        $promotion = new Promotion();
+        $promotion->setName('Buy one get one half price ');
+        $promotion->setAdjustment(0.75);
+        $promotion->setCriteria(["minimum_quantity" => 2]);
+        $promotion->setType('even_items_multiplier');
+
+        $enquiry = new LowestPriceEnquiry();
+        $enquiry->setQuantity(5);
+
+        $evenItemsMultiplier = new EvenItemsMultiplier();
+
+        // When
+        $modifiedPrice = $evenItemsMultiplier->modify(100, 5, $promotion, $enquiry);
+
+        // Then
+        $this->assertEquals(400, $modifiedPrice);
     }
 }
