9c40fecab561f32c317d0934464e3e4e1ef8818c
Daniel Yousefi Far <daniyousefifar@gmail.com>
2024-07-21 08:22:57 +0300

    01-Routing

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProductsController
+{
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id): Response
+    {
+        if ($request->headers->has('force-fail')) {
+            return new JsonResponse([
+                'error' => 'Promotions Engine failure message'
+            ], $request->headers->get('force-fail'));
+        }
+
+        return new JsonResponse([
+            'quantity' => 5,
+            'request_location' => 'UK',
+            'voucher_code' => 'OU812',
+            'request_date' => '2024-07-21',
+            'product_id' => $id,
+            'price' => 100,
+            'discounted_price' => 50,
+            'promotion_id' => 3,
+            'promotion_name' => 'Black Friday half price sale',
+        ], 200);
+    }
+}
